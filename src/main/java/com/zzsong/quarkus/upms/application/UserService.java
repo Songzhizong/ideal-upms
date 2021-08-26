d63f0293ea77e76f4473741c52b427e06693ba87
宋志宗 <zzsong91@163.com>
2021-08-26 20:36:27 +0800

    update

---
+++ src/main/java/com/zzsong/quarkus/upms/application/UserService.java
@@ -0,0 +1,4 @@
+package com.zzsong.quarkus.upms.application;/**
+ * @author 宋志宗 on 2021/8/26
+ */public class UserService {
+}
---
+++ src/main/java/com/zzsong/quarkus/upms/domain/model/user/CertificateEnum.java
@@ -0,0 +1,4 @@
+package com.zzsong.quarkus.upms.domain.model.user;/**
+ * @author 宋志宗 on 2021/8/26
+ */public enum CertificateEnum {
+}
